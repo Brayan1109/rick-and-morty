4fbe144a784d332d37dd400f29a08e372e4bab0c
barroyo <barroyo1109@gmail.com>
2022-04-30 12:02:58 -0500

    version 2.0.3

--- src/componentes/ResidentInfo.js
+++ src/componentes/ResidentInfo.js
@@ -1,40 +1,53 @@
 import { useEffect, useState } from "react";
 import getResidents from "../services/getResidents";
-import './ResidentInfo.css';
-import jarLoading from '../assets/img/jar-loading.gif';
+import "./ResidentInfo.css";
+import jarLoading from "../assets/img/jar-loading.gif";
 
+function ResidentInfo({ infoResident }) {
+  const [resident, setResident] = useState(false);
+  const [isMove, setMove] = useState(false);
 
-function ResidentInfo({infoResident}){
+  useEffect(() => {
+    getResidents(infoResident).then((req) => {
+      setResident(req.data);
+    });
+  }, [infoResident]);
 
-    const [resident, setResident] = useState(false);
+  return (
+    <>
+      <div
+        onMouseOver={() => {
+          setMove(true);
+        }}
+        onMouseOut={() => {
+          setMove(false);
+        }}
+      >
+        {resident ? (
+          <div className={`container-resident ${isMove && "isMove"}`}>
+            <img src={resident.image} alt="imageresident" />
 
-    useEffect(() => {
-        getResidents(infoResident).then(req => {
-            setResident(req.data)
-        })
-    }, [infoResident])
-    
-    return(
-        <>  
-            {
-                (resident) ? <div className="container-resident">
-                                <img src={resident.image} alt='imageresident'/>
-
-                                <div className="container-resident-info">
-                                    <p><b>Nombre:</b> {resident.name}</p>
-                                    <p><b>Origen: </b> {resident.origin.name}</p>
-                                    <p><b>Status:</b> {resident.status}</p>
-                                    <p><b>Cant. Epis.</b> {resident.episode.length}</p>
-
-                                </div>
-                            </div>
-
-                            : <img className="loader" src={jarLoading} alt='loading'/>
-            }
-
-        </>
-    )
+            <div className="container-resident-info">
+              <p>
+                <b>Nombre:</b> {resident.name}
+              </p>
+              <p>
+                <b>Origen: </b> {resident.origin.name}
+              </p>
+              <p>
+                <b>Status:</b> {resident.status}
+              </p>
+              <p>
+                <b>Cant. Epis.</b> {resident.episode.length}
+              </p>
+            </div>
+          </div>
+        ) : (
+          <img className="loader" src={jarLoading} alt="loading" />
+        )}
+      </div>
+    </>
+  );
 }
-
 
 export default ResidentInfo;
